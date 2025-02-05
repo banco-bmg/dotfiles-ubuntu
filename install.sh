0abaea7b28c55c8a3267fe53b03541094959927f
zandler.souza <zandler@outlook.com>
2025-02-05 17:22:50 -0300

    ok

---
+++ install.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -e
+
+clone_repo() {
+    git clone https://github.com/zandler/dotfiles ~/.dotfiles
+}
+
+exec_bootstrap() {
+    cd $HOME/.dotfiles 
+    chmod +x bootstrap.sh
+    chmod +x install.sh 
+    ./bootstrap.sh
+}
+
+
+
+is_ubuntu() {
+    if grep -q "ID=ubuntu" /etc/os-release; then 
+        echo "Checked. OK "
+        clone_repo
+        exec_bootstrap
+    else 
+        echo "OS not ubuntu. exit. :/ "
+    fi
+}
+
+header() {
+
+    clear
+
+    echo -e "${ORANGE}
+     ###############################################
+     #   Dotfiles / Linux                          #
+     #   Author: Zandler <zandler@outlook.com>     #
+     #   Version: 0.9                              #
+     #   $(date +'%d/%m/%Y')                                # 
+     ###############################################${ENDCOLOR}"
+
+    sleep 5
+    clear
+}
+
+header 
+is_ubuntu
